a3f6f5ddaefe3b602b99bd4186fb201651239898
OlesiaElfimova219 <olesia.elfimova@stonybrook.edu>
2019-05-08 21:17:16 -0400

    Seller:reviews and showonlymine

--- src/SellerView.java
+++ src/SellerView.java
@@ -23,9 +23,7 @@ import java.awt.Panel;
 public class SellerView extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField itemsTxtField;
-	private JTextField ordersTxtField;
-	private JTextField shipmentsTxtField;
+	private JTextField textField;
 	private String sellerID;
 
 	/**
@@ -95,10 +93,10 @@ public class SellerView extends JFrame {
 		
 		itemsTable.setRowHeight(30);
 		
-		itemsTxtField = RowFilterUtil.createRowFilter(itemsTable);//JitemsTxtField();
-		itemsTxtField.setBounds(25, 52, 146, 26);
-		p1.add(itemsTxtField);
-		itemsTxtField.setColumns(10);
+		textField = RowFilterUtil.createRowFilter(itemsTable);//JTextField();
+		textField.setBounds(25, 52, 146, 26);
+		p1.add(textField);
+		textField.setColumns(10);
 		
 		itemsScrollPane.setOpaque(false);
 		itemsScrollPane.getViewport().setOpaque(false);
@@ -153,14 +151,13 @@ public class SellerView extends JFrame {
 				}
 			}
 		});
-		btnSaveRow.setBounds(612, 40, 115, 29);
+		btnSaveRow.setBounds(572, 40, 115, 29);
 		p1.add(btnSaveRow);
 		
-		JButton btnShowOnlyMy = new JButton("See Reviews");
-		btnShowOnlyMy.addActionListener(new ActionListener() {
+		JButton btnSeeR = new JButton("See Reviews");
+		btnSeeR.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-<<<<<<< HEAD
 					if(itemsTable.getSelectionModel().isSelectionEmpty() == true) {
 						JOptionPane.showMessageDialog(null, "Please select item for which you\nwould like to see reviews");
 					}else {
@@ -175,30 +172,17 @@ public class SellerView extends JFrame {
 					
 				} catch (Exception ex) {
 					// TODO Auto-generated catch block
-=======
-					
-					Scanner sc = new Scanner(new File("username_info.txt"));
-					int id = sc.nextInt();
-					
-					String query = "select * from mydb.item ";
-					query += "where SellerID=" + id;
-					PreparedStatement pst = connection.prepareStatement(query);
-					ResultSet rs = pst.executeQuery();
-				
-					itemsTable.setModel(DbUtils.resultSetToTableModel(rs));
-				} catch(Exception ex) {
->>>>>>> 3bc25ac986a13f9e13604d306a096299420e5544
 					ex.printStackTrace();
 				}
 			}
 		});
-		btnShowOnlyMy.setBounds(783, 40, 207, 29);
-		p1.add(btnShowOnlyMy);
+		btnSeeR.setBounds(701, 40, 207, 29);
+		p1.add(btnSeeR);
 		
-		JLabel lblItemsSearch = new JLabel("Search");
-		lblItemsSearch.setForeground(Color.WHITE);
-		lblItemsSearch.setBounds(15, 16, 69, 20);
-		p1.add(lblItemsSearch);
+		JLabel lblSearch = new JLabel("Search");
+		lblSearch.setForeground(Color.WHITE);
+		lblSearch.setBounds(15, 16, 69, 20);
+		p1.add(lblSearch);
 		
 		JButton btnRemove = new JButton("Remove");
 		btnRemove.addActionListener(new ActionListener() {
@@ -219,9 +203,9 @@ public class SellerView extends JFrame {
 					p1.add(btnRemove);
 					p1.add(btnAddRow);
 					p1.add(itemsScrollPane);
-					p1.add(btnShowOnlyMy);
-					p1.add(itemsTxtField);
-					p1.add(lblItemsSearch);
+					p1.add(btnSeeR);
+					p1.add(textField);
+					p1.add(lblSearch);
 					p1.add(btnSaveRow);
 					
 					String query = "select * from mydb.item";
@@ -251,25 +235,36 @@ public class SellerView extends JFrame {
 					System.out.println("Choose table and row");
 				}
 			}
-		}); 
+		});
 		
 		btnRemove.setBounds(271, 40, 115, 29);
 		p1.add(btnRemove);
 		
+		JButton btnShowOnlyMy = new JButton("Show Only My Items");
+		btnShowOnlyMy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Scanner sc = new Scanner(new File("username_info.txt"));
+					int id = sc.nextInt();
+					String query = "select * from mydb.item ";
+					query += "where SellerID=" + id;
+					PreparedStatement pst = connection.prepareStatement(query);
+					ResultSet rs = pst.executeQuery();
+					itemsTable.setModel(DbUtils.resultSetToTableModel(rs));
+
+				} catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnShowOnlyMy.setBounds(923, 40, 207, 29);
+		p1.add(btnShowOnlyMy);
+		
 		JPanel p2 = new JPanel();
 		tabbedPane.add("Orders",p2); 
 		
-		int id = 0;
-		
-		try {
-			Scanner sc = new Scanner(new File("username_info.txt"));
-			id = sc.nextInt();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		query = "select * from mydb.sendsout ";
-		query += "where sellerID =" + id;
+		query = "select * from mydb.sendsout;";
+		//query += "where sellerID = (the seller ID of current User);";
 		pst = connection.prepareStatement(query);
 		rs = pst.executeQuery();
 		
@@ -292,8 +287,6 @@ public class SellerView extends JFrame {
 		
 		ordersTable.setRowHeight(30);
 		
-		
-		
 		p2.setBackground(new Color(0,1,32));
 		p2.setForeground(new Color(0,1,32));
 		
@@ -302,28 +295,12 @@ public class SellerView extends JFrame {
 		p2.setLayout(null);
 		p2.add(ordersScrollPane);
 		
-		JLabel lblOrdersSearch = new JLabel("Search");
-		lblOrdersSearch.setForeground(Color.WHITE);
-		lblOrdersSearch.setBounds(15, 16, 69, 20);
-		p2.add(lblOrdersSearch);
-		
-		ordersTxtField = RowFilterUtil.createRowFilter(ordersTable);//JitemsTxtField();
-		ordersTxtField.setBounds(25, 52, 146, 26);
-		p2.add(ordersTxtField);
-		ordersTxtField.setColumns(10);
 		
 		JPanel p3 = new JPanel();
 		tabbedPane.add("Shipments",p3); 
 		
-		try {
-			Scanner sc = new Scanner(new File("username_info.txt"));
-			id = sc.nextInt();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		query = "select * from mydb.shipment ";
-		query += "where SellerID =" + id;
+		query = "select * from mydb.shipment;";
+		//query += "where sellerID = (the seller ID of current User);";
 		pst = connection.prepareStatement(query);
 		rs = pst.executeQuery();
 		
@@ -353,16 +330,6 @@ public class SellerView extends JFrame {
 		shipmentsScrollPane.getViewport().setOpaque(false);
 		p3.setLayout(null);
 		p3.add(shipmentsScrollPane);
-		
-		JLabel lblShipmentsSearch = new JLabel("Search");
-		lblShipmentsSearch.setForeground(Color.WHITE);
-		lblShipmentsSearch.setBounds(15, 16, 69, 20);
-		p3.add(lblShipmentsSearch);
-		
-		shipmentsTxtField = RowFilterUtil.createRowFilter(shipmentsTable);//JitemsTxtField();
-		shipmentsTxtField.setBounds(25, 52, 146, 26);
-		p3.add(shipmentsTxtField);
-		shipmentsTxtField.setColumns(10);
 		
 		contentPane.add(tabbedPane);
 		
